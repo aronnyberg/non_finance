cfaba657d1e8b747862ce2c3a5bf1d556f206923
Aron <nybergaron@gmail.com>
2022-11-19 18:58:30 +0000

    cleaned up

--- scrape_plate_data.py
+++ scrape_plate_data.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import time
 
 #dict to hold the data in
-adict = {"Item Title":[], "Item Price":[]}
+adict = {"Item Title":[], "Item Price":[], "Unix Timestamp (collected)":[]}
 #ebay page url, for loop adds page number
 ebayUrl = "https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw=20kg+weight+plates&_in_kw=1&_ex_kw=&_sacat=0&LH_Sold=1&_udlo=&_udhi=&_samilow=&_samihi=&_sadis=15&_sargn=-1%26saslc%3D1&_fsradio2=%26LH_LocatedIn%3D1&_salic=3&LH_SubLocation=1&_sop=13&_dmd=1&_ipg=60&LH_Complete=&_pgn="
 csvPath = '~/code/non_finance/weightPlateData.csv'
@@ -35,6 +36,7 @@ while toBreak == False:
             if str(each_price)[:1] == "£":
                 adict['Item Title'].append(each_title)
                 adict['Item Price'].append(each_price)
+                adict['Unix Timestamp (collected)'].append(time.time())
             else:
                 #not sure why I don't get data after some point on the 4th page tried re-writing, oh well
                 pass
